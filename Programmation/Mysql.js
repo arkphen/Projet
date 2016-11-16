baea5d6f2e888b82779d10e2a6e3836fb9c90623
[arkphen] <[arkphen@gmail.com]>
2016-11-16 13:58:32 -0500

    Update

--- Programmation/Mysql.js
+++ Programmation/Mysql.js
@@ -1,5 +1,5 @@
 /**
- * Created by acharland on 11/16/16.
+ * Created by acharland on 11/9/16.
  */
 var mysql = require('mysql');
 
@@ -12,11 +12,22 @@ var connection = mysql.createConnection({
 
 
 connection.connect(function (err) {
+    if (err) throw err;
 
+
+    for (var i = 0; i< 1000; i++) {
+        var prenom = "User" + i;
+        var nom = "Last" + i;
+
+        connection.query(
+            {
+                sql: "delete from professeur where prenom=?",
+                values: [prenom]
+            },
             function (err, rows, fields) {
                 if (err) throw err;
 
-                console.log("ADDED");
+                console.log("REMOVE");
             });
     }
     connection.end();
