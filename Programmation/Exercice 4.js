093c56e257053613c9335a870379ba3cb24adcbf
[arkphen] <[arkphen@gmail.com]>
2016-11-26 14:03:56 -0500

    TP4 FINI !!!!

--- Programmation/Exercice 4.js
+++ Programmation/Exercice 4.js
@@ -1,19 +1,40 @@
-/**
- * Created by acharland on 11/26/16.
- */
-/** Heure **/
-var timeObj = new Date();
-var heures = timeObj.getHours();
-var minutes = timeObj.getMinutes();
-var secondes = timeObj.getSeconds();
-newtime = heures + ":" + minutes + ":" + secondes;
-console.log(newtime);
+//Lets require/import the HTTP module
+var http = require('http');
+
+//Lets define a port we want to listen to
+const PORT = 8080;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response) {
+
+    /** Heure **/
+    var timeObj = new Date();
+    var heures = timeObj.getHours();
+    var minutes = timeObj.getMinutes();
+    var secondes = timeObj.getSeconds();
+    newtime = heures + ":" + minutes + ":" + secondes;
 
 
-/** Date **/
-var dateObj = new Date();
-var mois = dateObj.getUTCMonth() + 1; //months from 1-12
-var jours = dateObj.getUTCDate();
-var annees = dateObj.getUTCFullYear();
-newdate = annees + "/" + mois + "/" + jours;
-console.log(newdate);
+    /** Date **/
+    var dateObj = new Date();
+    var mois = dateObj.getUTCMonth() + 1; //months from 1-12
+    var jours = dateObj.getUTCDate();
+    var annees = dateObj.getUTCFullYear();
+    newdate = annees + "/" + mois + "/" + jours;
+
+
+    if (request.url.endsWith("date")) {
+        response.end(newdate)
+    } else {
+        if (request.url.endsWith("time")) {
+            response.end(newtime);
+        }
+    }
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function () {
+
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
--- Programmation/Prexam-query.js
+++ Programmation/Prexam-query.js
@@ -1,39 +1,16 @@
-/**
- * Created by acharland on 11/23/16.
- */
-/**
- * Created by acharland on 11/23/16.
- */
+/** Created by acharland on 11/23/16.*/
 
 var http = require('http');
-var mysql = require('mysql');
 
 const PORT =8080;
-var req = {
-    "Ligne_facture" : "select * from Ligne_facture where id = ?",
-};
 
 function handleRequest(request, response) {
 
-    var url = request.url;
-    var fid = url.split("/")[1];
-    var montantt = url.split("/")[2];
-
-    var connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'acharland',
-        password: '',
-        database: 'prexam'
-    });
 
     connection.connect(function (err) {
         if (err) throw err;
         connection.query(
-            {
-                sql: req[fid],
-                values: [montantt]
 
-            },
             function (err, rows) {
                 if (err) throw err;
 
