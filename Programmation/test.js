96faeaf7c0a7822f40f27a019dbce9a1d3ed0ad0
[arkphen] <[arkphen@gmail.com]>
2016-11-29 14:14:09 -0500

    TP1 sur test up

--- Programmation/test.js
+++ Programmation/test.js
@@ -1,47 +1,37 @@
-/**
- * Created by acharland on 11/28/16.
- */
-var http = require('http');
-var mysql = require('mysql');
-var url = require('url');
-const PORT = 8080;
-var req =('SELECT * FROM Liste_Clients');
+// Include http module,
+var http = require('http'),
+// And mysql module you've just installed.
+    mysql = require("mysql");
 
-
-function handleRequest(request, response) {
-
-    var url = request.url;
-    var ID_Client = url.split("/")[1];
-
-    var connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'acharland',
-        password: '',
-        database: 'Agence_de_location'
-    });
-
-    connection.connect(function (err) {
-        if (err) throw err;
-
-        connection.query(
-            {
-                sql: "SELECT * FROM Liste_Clients WHERE ID_Client = ?",
-                values: [ID_Client]
-
-            },
-            function (err, rows, fields) {
-                if (err) throw err;
-
-                response.end(JSON.stringify);
-                connection.end();
-            });
-    });
-
-
-}
-var server = http.createServer(handleRequest);
-
-server.listen(PORT, function () {
-
-    console.log("Server listening on: http://localhost:%s", PORT);
+// Create the connection.
+// Data is default to new mysql installation and should be changed according to your configuration.
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'acharland',
+    password: '',
+    database: 'Agence_de_location'
 });
+
+// Create the http server.
+http.createServer(function (request, response) {
+    // Attach listener on end event.
+    request.on('end', function () {
+       // if (request.url == '/') {
+        // Query the database.
+        connection.query('SELECT * FROM Liste_Clients;', function (error, rows, fields) {
+            response.writeHead(200, {
+                'Content-Type': 'x-application/json'
+            });
+            // Send data as JSON string.
+            // Rows variable holds the result of the query.
+            response.end(JSON.stringify(rows));
+        });
+        //} else {
+            // Indicate that requested file was not found.
+        //    response.writeHead(404);
+            // And end request without sending any data.
+        //    response.end();
+        }
+);
+// Listen on the 8080 port.
+}).listen(8080);
